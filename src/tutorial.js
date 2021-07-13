6a261bb4d399c219af566da9b50b75a7dddce7a4
mp-gumi <basil725woodstock@gmail.com>
2021-07-13 15:13:39 +0900

    prettierでformat

--- src/tutorial.js
+++ src/tutorial.js
@@ -1,22 +1,15 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 export const MyComponent = () => {
-    const [count, setCount] = useState(0);
-    //useStateは、クラスや関数内で宣言する
-    return(
-        <div>
-            <p>
-                <button onClick={()=> setCount(count + 1)}>
-                Push me!
-                </button>
-                You pushed {count} times!
-            </p>
-            <input
-             placeholder={'Your Name'}
-             >
-
-            </input>
-        </div>
-    )
-}
-
+  const [count, setCount] = useState(0);
+  //useStateは、クラスや関数内で宣言する
+  return (
+    <div>
+      <p>
+        <button onClick={() => setCount(count + 1)}>Push me!</button>
+        You pushed {count} times!
+      </p>
+      <input placeholder={"Your Name"}></input>
+    </div>
+  );
+};
