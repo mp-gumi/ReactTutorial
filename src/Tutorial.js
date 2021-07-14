864a31965287ffa7149b51a89079d0e679a365fc
mp-gumi <basil725woodstock@gmail.com>
2021-07-14 11:54:06 +0900

    背景色の設定 useStateを使った背景色の変更

--- src/Tutorial.js
+++ src/Tutorial.js
@@ -3,9 +3,21 @@ import React, { useState } from "react";
 export const Tutorial = () => {
   const [count, setCount] = useState(0);
   //useStateは、クラスや関数内で宣言する
+  const lightOn = "#FFFF48";
+  const lightOff = "#000000";
+
+  const [light, setLight] = useState(lightOn);
+  const divStyle = { backgroundColor: light };
+  function toggleLight() {
+    if (light === lightOn) {
+      setLight(lightOff);
+    } else {
+      setLight(lightOn);
+    }
+  }
 
   return (
-    <div className="Tutorial">
+    <div className="Tutorial" style={divStyle}>
       <h1>My Profile</h1>
       <p>
         <button onClick={() => setCount(count + 1)}>Push me!</button>
@@ -13,7 +25,7 @@ export const Tutorial = () => {
       <p>You pushed {count} times!</p>
       <input placeholder={"Your Name"} />
       <p>
-        Please Follow me on
+        Please Follow me on　
         <a
           href={"https://twitter.com/mp_gumi"}
           target="_blank" //リンクを別タブで開く
@@ -23,8 +35,9 @@ export const Tutorial = () => {
         </a>
       </p>
       <img
-        src="https://1.bp.blogspot.com/-uo_TRiIGFsQ/X9lJhr3IuFI/AAAAAAABc6M/u2rxPd_7ED4mhwmywANaBAbe0RisJ-9pwCNcBGAsYHQ/s608/money_kinbut.png"
-        alt={"money-pig"}
+        onClick={toggleLight}
+        src="https://2.bp.blogspot.com/-QpIzYIhX_R0/VrN1H-ENQYI/AAAAAAAA3xU/v8lJEBoRMGA/s800/denkyuu_on.png"
+        alt={"light_bulb"}
         width={"200px"}
       />
     </div>
