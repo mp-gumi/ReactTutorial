d66acaba404dcb4d628b7b0fcd6dd61d10e8e904
mp-gumi <basil725woodstock@gmail.com>
2021-07-14 19:38:39 +0900

    画像クリックで表示される画像が変更されるように

--- src/Tutorial.js
+++ src/Tutorial.js
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
 
 export const Tutorial = () => {
-  const [count, setCount] = useState(0);
   //useStateは、クラスや関数内で宣言する
+  const [count, setCount] = useState(0);
+
+  //ボタンのクリック回数が5以上のときに表示する内容を変化させる
+  function countCheck(count) {
+    let returnText;
+    if (count < 5) {
+      returnText = "Push upper button 5 times.";
+    } else {
+      returnText = 'The answer is "WATER".';
+    }
+    return returnText;
+  }
+  //色の設定とオンオフ設定
   const lightOn = "#FFFF48";
   const lightOff = "#000000";
-
   const [light, setLight] = useState(lightOn);
   const divStyle = { backgroundColor: light };
   function toggleLight() {
@@ -14,6 +25,16 @@ export const Tutorial = () => {
     } else {
       setLight(lightOn);
     }
+  }
+
+  //電気のオンオフで表示する画像を変更
+  let srcLight;
+  if (light === lightOn) {
+    srcLight =
+      "https://2.bp.blogspot.com/-QpIzYIhX_R0/VrN1H-ENQYI/AAAAAAAA3xU/v8lJEBoRMGA/s800/denkyuu_on.png";
+  } else {
+    srcLight =
+      "https://2.bp.blogspot.com/-bB1KTaBEAm0/VrN1H-lrLFI/AAAAAAAA3xQ/2HKUMC-7kGw/s800/denkyuu_off.png";
   }
 
   return (
@@ -42,14 +63,14 @@ export const Tutorial = () => {
       </ul>
       <img
         onClick={toggleLight}
-        src="https://2.bp.blogspot.com/-QpIzYIhX_R0/VrN1H-ENQYI/AAAAAAAA3xU/v8lJEBoRMGA/s800/denkyuu_on.png"
+        src={srcLight}
         alt={"light_bulb"}
-        width={"200px"}
+        width={"100px"}
       />
 
       <p style={{ color: lightOn }}>
         <input placeholder={"Find Answer"} />
-        Push upper button 5 times.
+        {countCheck()}
       </p>
     </div>
   );
